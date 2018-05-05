1171b065a7c84a6260b856e99808147d2f3fc6d8
Jacek Wieczorek <mrjjot@gmail.com>
2018-05-05 13:35:52 +0200

    GLSL matrices chaneg

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -3,7 +3,8 @@ layout(location = 0) in vec3 vPosition; //Vertex position (modelspace)
 layout(location = 1) in vec3 vColor;    //Vertex color
 
 //Model, view and projection matrices
-uniform mat4 MVP;
+uniform mat4 model;
+uniform mat4 camera;
 
 //Fragment color output
 out vec3 fColor;
@@ -11,7 +12,7 @@ out vec3 fColor;
 void main( )
 {
 	//Project vertex
-    gl_Position = MVP * vec4(vPosition, 1);
+    gl_Position = camera * model * vec4(vPosition, 1);
 
 	//Just pass on the color to the fragment shader
 	fColor = vColor;
