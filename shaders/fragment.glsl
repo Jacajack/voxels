6db225e5724c2bb340627446e619e2946d60056f
Jacek Wieczorek <mrjjot@gmail.com>
2018-05-06 20:10:44 +0200

    Add ambient-like fake lighting (it adds more depth, really..)

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -2,14 +2,29 @@
 
 //Color from vertex shader
 //Output color
-in vec2 UV;
+in vec2 v_uv_o;
 out vec3 color;
+
+
+in vec3 v_eye_camera;
+in vec3 v_normal_camera;
+in vec3 v_pos_world;
+in mat4 mat_view;
 
 uniform sampler2D texture_sampler;
 
 void main()
 {
+	float cosTheta = abs(dot(normalize(v_normal_camera), normalize(v_eye_camera)));
+
+
+	//vec3 light = vec3( 5, 0, 0 );
+	//vec3 light_world = (mat_view * vec4(light, 1)).xyz;
+
+	//vec3 light_dir = v_pos_world - light_world;
+	f//loat theta = dot(normalize(light_dir), normalize(v_normal_camera));
+
 	//Just apply the color from vertex shader
-    color = texture(texture_sampler, UV).rgb;
-	//vec3(1.0,1.0,1.0);//fColor;
+	color = texture(texture_sampler, v_uv_o).rgb * cosTheta;
+
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,20 +1,45 @@
 #version 330 core
-layout(location = 0) in vec3 vPosition; //Vertex position (modelspace)
-layout(location = 1) in vec2 vUV;       //Vertex UV
+layout(location = 0) in vec3 v_pos_model; //Vertex position (modelspace)
+layout(location = 1) in vec2 v_uv;        //Vertex UV
+layout(location = 2) in vec3 v_normal_model; //Vertex normal (modelspace)
 
 //Model, view and projection matrices
-uniform mat4 model;
-uniform mat4 camera;
+uniform mat4 mat_model;
+uniform mat4 mat_view;
+uniform mat4 mat_projection;
 
-//Fragment color output
-out vec2 UV;
+//UV map output
+out vec2 v_uv_o;
+
+//Vertex position in world and cameraspace
+out vec3 v_pos_world;
+out vec3 v_pos_camera;
+
+//Vector from vertex to the camera (in camera space)
+out vec3 v_eye_camera;
+out vec3 v_normal_camera;
 
 void main( )
 {
-	//Project vertex
-    gl_Position = camera * model * vec4(vPosition, 1);
+	//Camera transformation
+	mat4 mat_camera = mat_projection * mat_view;
+
+	//Vertex position in world
+	v_pos_world = (mat_model * vec4(v_pos_model, 1)).xyz;
+
+	//Veretx position in camera space (camera is at (0, 0))
+	v_pos_camera = (mat_view * vec4(v_pos_world, 1)).xyz;
+
+	//Position on screen	
+	gl_Position = mat_projection * vec4(v_pos_camera, 1);
+
+	//From vertex to camera
+	v_eye_camera = vec3(0, 0, 0) - v_pos_camera;
+	v_normal_camera = (mat_view * mat_model * vec4(v_normal_model, 0)).xyz;
+
 
 	//Just pass on the color to the fragment shader
-	//fColor = vColor;
-	UV = vUV;
+	v_uv_o = v_uv;
+
+	
 }
