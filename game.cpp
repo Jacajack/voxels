572b0165d22400fc05ab3cead819d1f5ff77976c
Jacek Wieczorek <mrjjot@gmail.com>
2018-05-06 22:44:40 +0200

    Add thread unsafe camera movement

--- game.cpp
+++ game.cpp
@@ -14,12 +14,118 @@
 
 #include "renderer.hpp"
 
+struct
+{
+    //Position and orientation
+    float x, y, z;
+    float pitch = 0, yaw = 0;
+
+    float speed = 10.0;
+
+    //Movement orders
+    bool fd = false, bw = false, rt = false, lt = false;
+
+    //Update function
+    void update( double dt )
+    {
+        //Move forward
+        if (fd)
+        {
+            z += sin(pitch) * speed * dt;
+            x += sin(yaw) * cos(pitch) * speed * dt;
+            y += cos(yaw) * cos(pitch) * speed * dt;
+        }
+
+        //Move backwards
+        if (bw)
+        {
+            z -= sin(pitch) * speed * dt;
+            x -= sin(yaw) * cos(pitch) * speed * dt;
+            y -= cos(yaw) * cos(pitch) * speed * dt;
+        }
+
+        //Move left
+        if (lt)
+        {
+            x -= sin(yaw + M_PI / 2.0 ) * speed * dt;
+            y -= cos(yaw + M_PI / 2.0) * speed * dt;
+        }
+
+        //Move right
+        if (rt)
+        {
+            x -= sin(yaw - M_PI / 2.0 ) * speed * dt;
+            y -= cos(yaw - M_PI / 2.0) * speed * dt;
+        }
+
+        yaw = ((double)renderer::mouse_x / renderer::window_width) * M_PI;
+        pitch = -(((double)renderer::mouse_y / renderer::window_height) - 0.5) * 3.14; //Note: this can't be full PI, because otherwise camera would flip
+
+        //Calculate position and direction and generate new view matrix
+        glm::vec3 pos = glm::vec3(x, y, z);
+        glm::vec3 dir = glm::vec3( sin(yaw) * cos(pitch), cos(yaw) * cos(pitch), sin(pitch) );
+        renderer::view_matrix = glm::lookAt(
+            pos, //Where it is?
+            pos + dir, //Where to look?
+            glm::vec3(0, 0, 1)  //Where is up?
+        );
+    }
+} camera;
+
+
+void keyboard_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_W && action == GLFW_PRESS)
+        camera.fd = true;
+    if (key == GLFW_KEY_W && action == GLFW_RELEASE)
+        camera.fd = false;
+
+    if (key == GLFW_KEY_S && action == GLFW_PRESS)
+        camera.bw = true;
+    if (key == GLFW_KEY_S && action == GLFW_RELEASE)
+        camera.bw = false;
+
+    if (key == GLFW_KEY_A && action == GLFW_PRESS)
+        camera.lt = true;
+    if (key == GLFW_KEY_A && action == GLFW_RELEASE)
+        camera.lt = false;
+
+     if (key == GLFW_KEY_D && action == GLFW_PRESS)
+        camera.rt = true;
+    if (key == GLFW_KEY_D && action == GLFW_RELEASE)
+        camera.rt = false;
+
+    if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+        camera.yaw +=  0.314;
+
+    if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+        camera.yaw -=  0.314;
+
+    if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+        camera.pitch +=  0.314;
+
+    if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+        camera.pitch -=  0.314;
+
+}
+
+
 
 int main( int argc, char **argv )
 {
     //Start renderer thread
     pthread_t renderer_thread;
     pthread_create( &renderer_thread, NULL, renderer::init, NULL );
+
+    camera.x = 0;
+    camera.y = 5;
+    camera.z = 0;
+ 
+ 
+    //Wait for render loop to start
+    while (!renderer::ready);
+
+    glfwSetKeyCallback(renderer::window, keyboard_callback);
 
     //Rotate camera around z axis
     struct timeval t0, t1, tdiff;
@@ -36,11 +142,7 @@ int main( int argc, char **argv )
         dt = tdiff.tv_sec + tdiff.tv_usec / 1000000.0;
         angle += dt * 3.14;
 
-	    renderer::view_matrix = glm::lookAt(
-			glm::vec3(6 * sin(angle), 6 * cos(angle), 3 * sin(angle / 4)), //Eye
-			glm::vec3(0, 0, 0), //Center
-			glm::vec3(0, 0, 1)  //Head orientation
-		);
+        camera.update(dt);
 
         //Delay
         struct timespec sleeptime;
