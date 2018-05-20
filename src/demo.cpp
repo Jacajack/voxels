39b490a8aff20f3959be88a2286e8e0d57396ade
Jacek Wieczorek <mrjjot@gmail.com>
2018-05-20 12:25:59 +0200

    Display white unicorn?

--- src/demo.cpp
+++ src/demo.cpp
@@ -7,6 +7,13 @@ int main( int argc, char **argv )
 	//Create window
 	if ( ifrit::init( 1024, 768, "Ifrit dev" ) ) return 1;
 	
+
+	//Create vertex array
+	GLuint vertex_array_id;
+	glGenVertexArrays(1, &vertex_array_id);
+	glBindVertexArray(vertex_array_id);
+
+
 	ifrit::Shader shader(
 		{
 			{"shaders/flat/vertex.glsl", GL_VERTEX_SHADER},
@@ -30,7 +37,7 @@ int main( int argc, char **argv )
 		glm::vec3( 0, 1, 0 )
 	);
 	glm::mat4 mat_projection = glm::perspective(
-		glm::radians( 45.0 ), //FoV
+		glm::radians( 90.0 ), //FoV
 		(double) 1024.0 / 768.0, //Aspect ratio
 		0.1, //Near clipping plane
 		100.0 //Far clipping plane
@@ -43,7 +50,7 @@ int main( int argc, char **argv )
 	glUniformMatrix4fv( shader.uniform( "mat_view" ), 1, GL_FALSE, &mat_view[0][0] );
 	glUniformMatrix4fv( shader.uniform( "mat_projection" ), 1, GL_FALSE, &mat_projection[0][0] );
 	glUniformMatrix4fv( shader.uniform( "mat_model" ), 1, GL_FALSE, &mat_model[0][0] );
-	glUniform1i( shader.uniform( "texture_sampler" ), 0);
+	glUniform1i( shader.uniform( "texture_sampler" ), 0 );
 
 
 
@@ -52,14 +59,15 @@ int main( int argc, char **argv )
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 	
-	//Create vertex array
-	GLuint vertex_array_id;
-	glGenVertexArrays(1, &vertex_array_id);
-	glBindVertexArray(vertex_array_id);
+
 
 	//Game loop
 	do
 	{
+		glClearColor( 0.2, 0.2, 0.0, 0.0 );
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+
 		unicorn.draw( );
 		ifrit::update( );
 	}
