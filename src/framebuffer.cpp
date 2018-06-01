5262144ccf1f6e41bca133bbde2c720a3a9ebb8d
Jacek Wieczorek <mrjjot@gmail.com>
2018-06-01 11:35:14 +0200

    Add some test code

--- src/framebuffer.cpp
+++ src/framebuffer.cpp
@@ -9,7 +9,7 @@ lobor::Framebuffer::operator GLuint( )
 }
 
 //Use framebuffer for reading/writing
-void lobor::Framebuffer::use( bool read, bool draw )
+void lobor::Framebuffer::bind( bool read, bool draw )
 {
 	if ( read && draw )
 		glBindFramebuffer( GL_FRAMEBUFFER, this->id );
@@ -19,6 +19,20 @@ void lobor::Framebuffer::use( bool read, bool draw )
 			glBindFramebuffer( GL_READ_FRAMEBUFFER, this->id );
 		else
 			glBindFramebuffer( GL_DRAW_FRAMEBUFFER, this->id );
+	}
+}
+
+//Use default framebuffer for reading/writing
+void lobor::Framebuffer::unbind( bool read, bool draw )
+{
+	if ( read && draw )
+		glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+	else
+	{
+		if ( read )
+			glBindFramebuffer( GL_READ_FRAMEBUFFER, 0 );
+		else
+			glBindFramebuffer( GL_DRAW_FRAMEBUFFER, 0 );
 	}
 }
 
@@ -78,6 +92,8 @@ lobor::Framebuffer::Framebuffer( int width, int height, int texture_count )
 	glGenFramebuffers( 1, &this->id );
 	glBindFramebuffer( GL_FRAMEBUFFER, this->id );
 
+	lobor::log( LOBOR_DEBUG, "FBO id: %ld", (long) this->id );
+
 	//Limits
 	if ( texture_count > 32 )
 	{
@@ -101,6 +117,23 @@ lobor::Framebuffer::Framebuffer( int width, int height, int texture_count )
 	lobor::Texture dtex( width, height, GL_FLOAT, GL_DEPTH_COMPONENT, GL_DEPTH_COMPONENT );
 	glFramebufferTexture2D( GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, dtex, 0 );
 	this->textures.push_back( dtex );
+
+	
+
+	GLuint t;
+	glGenTextures( 1, &t );
+	glBindTexture( GL_TEXTURE_2D, t );
+	glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, 1024, 768, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+	glFramebufferTexture2D( GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, t, 0 );
+
+	GLuint rbo;
+	glGenRenderbuffers( 1, &rbo );
+	glBindRenderbuffer( GL_RENDERBUFFER, rbo );
+	glRenderbufferStorage( GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 1024, 768 );
+	glFramebufferRenderbuffer( GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo );
+
 
 	lobor::log( LOBOR_INFO, "generated %dx%d framebuffer consisting of %ld textures", width, height, this->textures.size( ) );
 }
